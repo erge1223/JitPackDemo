c102d15c86ed0eb65b34a9e76860a8f6472ca895
kwh <758306135@qq.com>
2021-03-01 10:21:12 +0800

    add utils

---
+++ mylibrary/src/main/java/com/my/mylibrary/MyUTilsDemo.java
@@ -0,0 +1,14 @@
+package com.my.mylibrary;
+
+/**
+ * author : kwh
+ * e-mail : kwhlucky1618@163.com
+ * date   : 2021/3/1 10:19
+ */
+class MyUTilsDemo {
+
+    //设置获取方法的逻辑
+    public static void getMethod(){
+
+    }
+}
