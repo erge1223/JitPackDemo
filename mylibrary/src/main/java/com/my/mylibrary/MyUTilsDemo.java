224218506eefb854a518dab98a6df8723677eeca
kwh <758306135@qq.com>
2021-03-01 10:33:58 +0800

    gradle tools version 3.0.1

--- mylibrary/src/main/java/com/my/mylibrary/MyUTilsDemo.java
+++ mylibrary/src/main/java/com/my/mylibrary/MyUTilsDemo.java
@@ -5,7 +5,7 @@ package com.my.mylibrary;
  * e-mail : kwhlucky1618@163.com
  * date   : 2021/3/1 10:19
  */
-class MyUTilsDemo {
+public class MyUTilsDemo {
 
     //设置获取方法的逻辑
     public static void getMethod(){
